481083560ffc17b67f044d668d5bb40a488fee0b
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2022-09-16 09:13:39 -0600

    Regenerate default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
-{ lib, mkDerivation
-, base, bytestring, cmdargs, containers, cryptohash, debian, directory
-, download-curl, fgl, filepath, MissingH, parsec, unix, zlib
+{ mkDerivation, base, bytestring, cmdargs, containers, cryptohash
+, curl, debian, directory, download-curl, fgl, filepath, lib
+, parsec, unix, zlib
 }:
 mkDerivation {
   pname = "RepoExplorer";
@@ -9,8 +9,8 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base bytestring cmdargs containers cryptohash debian directory
-    download-curl fgl filepath MissingH parsec unix zlib
+    base bytestring cmdargs containers cryptohash curl debian directory
+    download-curl fgl filepath parsec unix zlib
   ];
   description = "A set of utilities for exploring Debian package repositories";
   license = lib.licenses.mit;
