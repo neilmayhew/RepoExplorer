5011cef16f47f9a7b5f262e4554f8cb9b83ff12b
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2025-04-30 21:16:02 -0600

    Add a GitHub workflow for building debs

---
+++ build-deb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y --no-install-recommends \
+	devscripts fakeroot haskell-debian-utils \
+	libwww-perl file git
+
+git submodule update --init haskell-download-curl
+
+cd haskell-download-curl
+origtargz -u
+apt-get-build-depends -y
+debuild -b -us -uc
+debuild -- clean
+cd ..
+
+dpkg -i libghc-download-curl-dev_*.deb
+
+apt-get-build-depends -y
+debuild -b -us -uc
+debuild -- clean
